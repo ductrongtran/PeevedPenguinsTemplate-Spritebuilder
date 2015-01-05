1e3d17deb08f2fdf0756acfa2fc43d79f1954677
dtran <ductrongtran@me.com>
2015-01-05 14:30:32 +0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Duc Tran on 1/5/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,13 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Duc Tran on 1/5/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay
+
+@end
